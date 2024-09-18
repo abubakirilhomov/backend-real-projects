1857a1442c85bd2c86101264b1c60cb2ed22a53c
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-09-18 17:54:37 +0500

    safjoies

--- src/server.js
+++ src/server.js
@@ -84,10 +84,21 @@ io.on("connection", (socket) => {
 setInterval(cleanupOldValues, 5 * 60 * 1000); // Clean up every 5 minutes
 setInterval(updateProductsPeriodically, 5000); // Update products every 5 seconds
 
-const serviceAccount = require("../ServiceAccounts.json");
+// const serviceAccount = require("../ServiceAccounts.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Correctly format the private key
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  }),
 });
 
 // Define routes
